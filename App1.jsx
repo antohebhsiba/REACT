425e1ace21dc1f65044dbe0f476b440857eaadbe
antohebhsiba <antohebhsi@gmail.com>
2026-06-15 14:26:08 +0530

    Ex1

---
+++ App1.jsx
@@ -0,0 +1,23 @@
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Details</h1>
+
+      <Student
+        name="Anto"
+        age={19}
+        department="CSE"
+      />
+
+      <Student
+        name="John"
+        age={20}
+        department="IT"
+      />
+    </div>
+  );
+}
+
+export default App;
