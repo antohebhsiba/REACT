a5e1608b8d5daf0db38d80132eeabe8e2b9fad75
antohebhsiba <antohebhsi@gmail.com>
2026-06-15 15:05:04 +0530

    Ex 8

---
+++ App8.jsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Hello React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
